9a01d330bf1aa95fa6d933d6c24cf35b6cab95a4
Yash Jaiswal <bitorsic@protonmail.com>
2023-07-25 19:41:52 +0530

    Using environment variable for port

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,8 @@ app.use('/api/helpers', require('./helpers'));
 		console.log("[+] Connecting to MongoDB Atlas...");
 		await mongoose.connect(process.env.DB_URL);
 		console.log("[+] Connection Successful");
-		app.listen(5000, () => { console.log('[+] Server running on port 5000') });
+		const port = process.env.PORT || 3000;
+		app.listen(port, () => { console.log('[+] Server running on port ' + port) });
 	} catch (e) {
 		console.log("[-] Connection Failed");
 	}
