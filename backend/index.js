b0f484641f2741b4a03a8ecabce1b0f6d606c9ea
bitorsic <bitorsic@gmail.com>
2025-03-24 11:31:43 +0530

    io cors patch

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,12 @@ app.use('/helpers', require('./helpers'));
 
 const server = http.createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+	cors: {
+		origin: process.env.FRONTEND_URL,
+		methods: ["GET", "POST"],
+	},
+});
 
 (async () => {
 	try {
